4fc60479f1f59e45cf2ec4b7e0b942632d2e3178
Lucas Graf <lucasgraf01@gmail.com>
2026-03-03 21:14:09 +0100

    feat: add and integrate new application footer component

--- src/app/features/home/home-component.ts
+++ src/app/features/home/home-component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { SurveyCardsComponent } from './survey-cards/survey-cards.component';
 import { Categories } from "./categories/categories.component";
+import { Footer } from "../../core/layout/footer/footer";
 
 @Component({
   selector: 'app-home-component',
-  imports: [HeroComponent, SurveyCardsComponent, Categories],
+  imports: [HeroComponent, SurveyCardsComponent, Categories, Footer],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
 })
