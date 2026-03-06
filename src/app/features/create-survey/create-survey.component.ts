c526ea85e0465a6d929eae19d8867ebb2a5f8999
Lucas Graf <lucasgraf01@gmail.com>
2026-03-06 18:25:45 +0100

    feat: Enable dynamic creation and management of survey questions and their options with reactive forms, new UI components, and corresponding styling, while removing question item tests

--- src/app/features/create-survey/create-survey.component.ts
+++ src/app/features/create-survey/create-survey.component.ts
@@ -1,17 +1,18 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators, FormArray } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { PollService } from '../../core/services/poll.service';
 import { dateValidator, SurveyDataComponent } from './survey-data/survey-data.component';
 import { BadgeComponent } from '../../shared/ui/badge/badge';
 import { ButtonComponent } from '../../shared/ui/button/button';
+import { QuestionItemComponent } from "../create-survey/question-item/question-item.component";
 
 @Component({
   selector: 'app-create-survey',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink, SurveyDataComponent, BadgeComponent, ButtonComponent],
+  imports: [CommonModule, ReactiveFormsModule, SurveyDataComponent, BadgeComponent, ButtonComponent, QuestionItemComponent],
   templateUrl: './create-survey.component.html',
   styleUrl: './create-survey.component.scss',
 })
@@ -29,12 +30,38 @@ export class CreateSurveyComponent implements OnInit {
     description: [''],
     expires_at: ['', [dateValidator()]],
     category: ['', Validators.required],
-    questions: this.fb.array([])
+    questions: this.fb.array<any>([])
   });
+
+  get questions() {
+    return this.surveyForm.get('questions') as FormArray;
+  }
 
   ngOnInit() {
     this.pollService.fetchCategories();
+    this.addQuestion();
   }
+
+  addQuestion() {
+  const questionGroup = this.fb.group({
+    question_text: ['', Validators.required],
+    allow_multiple: [false],
+    options: this.fb.array([
+      this.fb.group({ label: ['', Validators.required] }),
+      this.fb.group({ label: ['', Validators.required] })
+    ])
+  });
+  
+  this.questions.push(questionGroup);
+}
+
+
+  removeQuestion(index: number) {
+    if (this.questions.length > 1) {
+      this.questions.removeAt(index);
+    }
+  }
+
 
   cancel() {
     this.router.navigate(['/']);
--- src/app/features/create-survey/question-item/question-item.component.ts
+++ src/app/features/create-survey/question-item/question-item.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlContainer, FormArray, FormGroup, FormGroupDirective, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
+import { InputFieldComponent } from '../../../shared/components/input-field/input-field.component';
+import { ButtonComponent } from '../../../shared/ui/button/button';
 
 @Component({
-  selector: 'app-question-item.component',
-  imports: [],
+  selector: 'app-question-item',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, InputFieldComponent, ButtonComponent],
   templateUrl: './question-item.component.html',
   styleUrl: './question-item.component.scss',
+  viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
 })
 export class QuestionItemComponent {
+  private parentContainer = inject(ControlContainer);
+  index = input.required<number>();
+  removeQuestion = output<number>();
 
+  get questionsArray() {
+    return (this.parentContainer as FormGroupDirective).form.get('questions') as FormArray;
+  }
+
+  get questionGroup() {
+    return this.questionsArray.at(this.index()) as FormGroup;
+  }
+
+  get optionsArray() {
+    return this.questionGroup.get('options') as FormArray;
+  }
+
+  addOption() {
+    if (this.optionsArray.length < 6) { 
+      this.optionsArray.push(new FormGroup({
+        label: new FormControl('', Validators.required)
+      }));
+    }
+  }
+
+  removeOption(oIdx: number) {
+    if (this.optionsArray.length > 2) {
+      this.optionsArray.removeAt(oIdx);
+    }
+  }
+
+  getOptionControl(oIdx: number): FormControl<string> {
+    return this.optionsArray.at(oIdx).get('label') as FormControl<string>;
+  }
 }
